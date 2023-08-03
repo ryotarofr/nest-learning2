25e0bf9791a8a5370fd1ad97f70cbfacc4c5a041
ryotarofr <ryoryo.fr0608@icloud.com>
2023-08-03 16:10:22 +0900

    ➕ default sigunup rogic

---
+++ src/user/dtos/auth.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString, Matches, MinLength } from "class-validator"
+
+
+export class SignupDto {
+  @IsString({ message: "有効な文字列を入力してください" })
+  @IsNotEmpty({ message: "入力必須項目です" })
+  name: string
+
+  @Matches(/^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/, { message: "利用可能な番号を入力してください" })
+  phone: string
+
+  @IsEmail()
+  email: string
+
+  @IsString()
+  @MinLength(5, { message: "5文字以上にしてください" })
+  password: string
+}
